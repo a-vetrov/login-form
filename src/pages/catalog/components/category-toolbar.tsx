ce7cf618b18ab67adec5207c448895d1afe7dcc6
Andrey Vetrov <a.vetrov@gmail.com>
2024-04-02 17:47:54 +0300

    Add NoSsr to CategoryToolbar

--- src/pages/catalog/components/category-toolbar.tsx
+++ src/pages/catalog/components/category-toolbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Chip, Stack } from '@mui/material'
+import {Chip, NoSsr, Stack} from '@mui/material'
 import { Link, NavLink } from 'react-router-dom'
 
 const categoriesList = [
@@ -15,20 +15,20 @@ const categoriesList = [
 
 export const CategoryToolbar: React.FC = () => {
   return (
-    <Stack marginTop={2} marginBottom={2} direction="row" spacing={2}>
-      {categoriesList.map((item) => (
-        <NavLink to={item.link} key={item.title}>
-          {({ isActive }) => (
-            <Chip
-              label={item.title}
-              component={Link}
-              to={item.link}
-              clickable
-              variant={isActive ? 'outlined' : 'filled'}
-            />
-          )}
-        </NavLink>
-      ))}
-    </Stack>
+    <NoSsr>
+      <Stack marginTop={2} marginBottom={2} direction="row" spacing={2}>
+        {categoriesList.map((item) => (
+          <NavLink to={item.link} key={item.title}>
+            {({ isActive }) => (
+              <Chip
+                label={item.title}
+                clickable
+                variant={isActive ? 'outlined' : 'filled'}
+              />
+            )}
+          </NavLink>
+        ))}
+      </Stack>
+    </NoSsr>
   )
 }
