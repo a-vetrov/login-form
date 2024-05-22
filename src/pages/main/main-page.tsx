bb9d6f4c3c1406e1fa3f43146012701f1ae5b7d1
Andrey Vetrov <a.vetrov@gmail.com>
2024-05-22 15:16:20 +0300

    Markets widget added to main page

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -1,36 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { userInfoSelector } from '../../store/selectors/user-info.ts'
+import React, { useMemo } from 'react'
+import { userInfoSelector } from '../../store/selectors/user-info'
 import { useSelector } from 'react-redux'
 import { MainToolbar } from '../../components/main-toolbar'
+import { Container, Typography } from '@mui/material'
+import { MarketWidget } from './market-widget'
 
 export const MainPage: React.FC = () => {
   const userInfo = useSelector(userInfoSelector)
 
+  const greetingMessage = useMemo(() => {
+    if (userInfo?.name) {
+      return `
+        Привет, ${userInfo.name}! Тут будет какая-то осмысленная подпись. Можно даже выдать инвест идею от Chat GPT.
+      `
+    }
+    return 'Добро пожаловать и всякое такое... '
+  }, [userInfo?.name])
+
   return (
     <>
       <MainToolbar />
-        <h1>Main page</h1>
-        <ul>
-          {
-            userInfo !== null
-              ? (
-                  <li>
-                    <Link to='/logout'>Logout</Link>
-                  </li>
-                )
-              : (
-                  <>
-                    <li>
-                      <Link to='/login'>Login</Link>
-                    </li>
-                    <li>
-                      <Link to='/register'>Register</Link>
-                    </li>
-                  </>
-                )
-          }
-        </ul>
+      <Container component="main" maxWidth="lg" sx={{ mt: 4 }}>
+        <Typography variant="h2">
+          Главная страница
+        </Typography>
+        <Typography variant="body1">
+          {greetingMessage}
+        </Typography>
+
+        <Typography variant="h3" marginY={2}>
+          Обзор рынков
+        </Typography>
+        <MarketWidget />
+      </Container>
     </>
   )
 }
--- src/utils/hooks/use-user-info.ts
+++ src/utils/hooks/use-user-info.ts
@@ -1,8 +1,8 @@
 import { useLazyGetUserInfoQuery } from '../../services/login'
 import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { userInfoSelector } from '../../store/selectors/user-info'
-import { type UserInfo } from '../../store/slices/user-slice'
+import { type UserInfo, userInfoSlice } from '../../store/slices/user-slice'
 
 interface UseUserInfoReturnType {
   userInfo: UserInfo | null
@@ -15,19 +15,26 @@ interface UseUserInfoReturnType {
  */
 export const useUserInfo = (): UseUserInfoReturnType => {
   const userInfo = useSelector(userInfoSelector)
-  const [trigger, { data, isLoading, isUninitialized }] = useLazyGetUserInfoQuery()
+  const [trigger, { data, isFetching, isUninitialized, error }] = useLazyGetUserInfoQuery()
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    if (userInfo == null) {
+    if (userInfo == null && !isFetching) {
       void trigger()
     }
   }, [userInfo])
+
+  useEffect(() => {
+    if (!isFetching && data?.userInfo && !userInfo) {
+      dispatch(userInfoSlice.actions.update(data.userInfo))
+    }
+  }, [error, data])
 
   const result = userInfo ?? data?.userInfo ?? null
 
   return {
     userInfo: result,
-    isLoading: isLoading || (isUninitialized && result === null),
+    isLoading: isFetching || (isUninitialized && result === null),
     isAuth: result !== null
   }
 }
