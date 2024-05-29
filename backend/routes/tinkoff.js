b7bc7f8f8d936b30435805948c61c71659304e9a
Andrey Vetrov <a.vetrov@gmail.com>
2024-05-29 16:08:06 +0300

    Sandbox paiIn finished

--- backend/routes/tinkoff.js
+++ backend/routes/tinkoff.js
@@ -1,15 +1,15 @@
-import express from "express";
-import {ensureLoggedIn} from "../handlers/ensure-logged-in.js";
-import {getUserById, TokenType} from "../db/models/user.js";
-import {sendError} from "../handlers/error.js";
+import express from 'express'
+import { ensureLoggedIn } from '../handlers/ensure-logged-in.js'
+import { getUserById, TokenType } from '../db/models/user.js'
+import { sendError } from '../handlers/error.js'
 // https://github.com/vitalets/tinkoff-invest-api
 // https://tinkoff.github.io/investAPI/sandbox/
-import {TinkoffApiError, TinkoffInvestApi} from "tinkoff-invest-api";
-import {PortfolioRequest_CurrencyRequest} from "tinkoff-invest-api/cjs/generated/operations.js";
-import {getFirstRealToken, getFirstSandboxToken} from "../utils/tokens.js";
-import {getBondsData, mergeWithMOEXData} from "../utils/moex.js";
+import { TinkoffApiError, TinkoffInvestApi } from 'tinkoff-invest-api'
+import { PortfolioRequest_CurrencyRequest } from 'tinkoff-invest-api/cjs/generated/operations.js'
+import { getFirstRealToken, getFirstSandboxToken } from '../utils/tokens.js'
+import { getBondsData, mergeWithMOEXData } from '../utils/moex.js'
 
-export const tinkoffRouter = express.Router();
+export const tinkoffRouter = express.Router()
 
 tinkoffRouter.get('/api/portfolio', ensureLoggedIn, async (req, res) => {
   try {
@@ -20,16 +20,16 @@ tinkoffRouter.get('/api/portfolio', ensureLoggedIn, async (req, res) => {
       return sendError(res, 403, 'Ошибка', 'Не удалось найти подходящий токен')
     }
 
-    const api = new TinkoffInvestApi({ token: token.token });
+    const api = new TinkoffInvestApi({ token: token.token })
 
-    const {accounts } = await api.users.getAccounts({});
+    const { accounts } = await api.users.getAccounts({})
 
     const portfolio = await api.operations.getPortfolio({
       accountId: accounts[0].id,
       currency: PortfolioRequest_CurrencyRequest.RUB
-    });
+    })
 
-    res.status(200).send({ success: true, data: {accounts, portfolio} });
+    res.status(200).send({ success: true, data: { accounts, portfolio } })
   } catch (error) {
     console.log('error', error)
     sendError(res, 403, 'Ошибка', error.details ?? 'Что-то пошло не так')
@@ -45,11 +45,11 @@ tinkoffRouter.get('/api/sandbox/accounts', ensureLoggedIn, async (req, res) => {
       return sendError(res, 403, 'Ошибка', 'Не удалось найти подходящий токен')
     }
 
-    const api = new TinkoffInvestApi({ token: token.token });
+    const api = new TinkoffInvestApi({ token: token.token })
 
-    const {accounts } = await api.sandbox.getSandboxAccounts({});
+    const { accounts } = await api.sandbox.getSandboxAccounts({})
 
-    res.status(200).send({ success: true, data: {accounts} });
+    res.status(200).send({ success: true, data: { accounts } })
   } catch (error) {
     console.log('error', error)
     sendError(res, 403, 'Ошибка', error.details ?? 'Что-то пошло не так')
@@ -65,10 +65,10 @@ tinkoffRouter.post('/api/sandbox/accounts/create', ensureLoggedIn, async (req, r
       return sendError(res, 403, 'Ошибка', 'Не удалось найти подходящий токен')
     }
 
-    const api = new TinkoffInvestApi({ token: token.token });
+    const api = new TinkoffInvestApi({ token: token.token })
 
-    const data = await api.sandbox.openSandboxAccount({});
-    res.status(200).send({ success: true, data });
+    const data = await api.sandbox.openSandboxAccount({})
+    res.status(200).send({ success: true, data })
   } catch (error) {
     console.log('error', error)
     sendError(res, 403, 'Ошибка', error.details ?? 'Что-то пошло не так')
@@ -84,11 +84,13 @@ tinkoffRouter.post('/api/sandbox/accounts/add-money', ensureLoggedIn, async (req
       return sendError(res, 403, 'Ошибка', 'Не удалось найти подходящий токен')
     }
 
-    const api = new TinkoffInvestApi({ token: token.token });
+    const api = new TinkoffInvestApi({ token: token.token })
 
-    const data = await api.sandbox.sandboxPayIn({accountId: req.body.id, amount: req.body.money});
+    const amount = api.helpers.toMoneyValue(req.body.money, 'RUB')
+
+    const data = await api.sandbox.sandboxPayIn({ accountId: req.body.id, amount })
     console.log('!!!!!!!!', data)
-    res.status(200).send({ success: true });
+    res.status(200).send({ success: true })
   } catch (error) {
     console.log('error', error)
     sendError(res, 403, 'Ошибка', error.details ?? 'Что-то пошло не так')
@@ -104,10 +106,10 @@ tinkoffRouter.delete('/api/sandbox/accounts/:id', ensureLoggedIn, async (req, re
       return sendError(res, 403, 'Ошибка', 'Не удалось найти подходящий токен')
     }
 
-    const api = new TinkoffInvestApi({ token: token.token });
+    const api = new TinkoffInvestApi({ token: token.token })
 
-    const data = await api.sandbox.closeSandboxAccount({accountId: req.params.id});
-    res.status(200).send({ success: true });
+    const data = await api.sandbox.closeSandboxAccount({ accountId: req.params.id })
+    res.status(200).send({ success: true })
   } catch (error) {
     console.log('error', error)
     sendError(res, 403, 'Ошибка', error.details ?? 'Что-то пошло не так')
@@ -115,12 +117,12 @@ tinkoffRouter.delete('/api/sandbox/accounts/:id', ensureLoggedIn, async (req, re
 })
 
 export const checkToken = async (token) => {
-  let result;
-  const api = new TinkoffInvestApi({ token: token.token });
+  let result
+  const api = new TinkoffInvestApi({ token: token.token })
   if (token.type === TokenType.real) {
-    result = await api.users.getAccounts({});
+    result = await api.users.getAccounts({})
   } else {
-    result = await api.sandbox.getSandboxAccounts();
+    result = await api.sandbox.getSandboxAccounts()
   }
   return result !== null
 }
