091d4b0f9900250f8c575532d78d01d6779d9c2d
Andrey Vetrov <a.vetrov@gmail.com>
2024-09-20 15:57:08 +0300

    Candles chart started

--- src/components/candle-stick-chart/candle-stick-chart.tsx
+++ src/components/candle-stick-chart/candle-stick-chart.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import * as Plot from '@observablehq/plot'
 import { marketDataApi } from '../../services/market-data'
 
 interface Props {
@@ -8,15 +9,32 @@ interface Props {
 /**
  * https://apexcharts.com/react-chart-demos/candlestick-charts/basic/
  * https://apexcharts.com/docs/react-charts/
+ * https://observablehq.com/@d3/candlestick-chart/2
  * @constructor
  */
 
 export const CandleStickChart: React.FC<Props> = ({ instrumentId }) => {
+  const containerRef = useRef(null)
   const { data } = marketDataApi.useGetCandlesQuery({ instrumentId })
+
+  useEffect(() => {
+    if (!data) return
+    const plot = Plot.plot({
+      y: { grid: true },
+      color: { scheme: 'burd' },
+      marks: [
+        Plot.ruleY([0]),
+        Plot.dot(data.candles, { x: 'time', y: 'volume'})
+      ]
+    })
+    containerRef.current.append(plot)
+    return () => { plot.remove() }
+  }, [data])
 
   return (
     <div>
       CandleStickChart {instrumentId}
+      <div ref={containerRef}/>
     </div>
   )
 }
