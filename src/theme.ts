21b0f60cc580fb36a864a15914a9ebff59853971
Andrey Vetrov <a.vetrov@gmail.com>
2024-10-16 17:23:20 +0300

    Customizing theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,30 @@
-import type { ThemeOptions } from '@mui/material/styles'
+import {createTheme, ThemeOptions} from '@mui/material/styles'
+
+const defaultTheme = createTheme();
 
 export const getCustomTheme = (): ThemeOptions => {
   return {
     palette: {
       mode: 'dark'
+    },
+    typography: {
+      h1: {
+        [defaultTheme.breakpoints.down('md')]: {
+          fontSize: '2rem',
+        },
+      },
+      h2: {
+        [defaultTheme.breakpoints.down('md')]: {
+          fontSize: '2rem',
+        },
+      },
+      h3: {
+        [defaultTheme.breakpoints.down('md')]: {
+          fontSize: '1.5rem',
+        },
+
+      },
+
     }
   }
 }
