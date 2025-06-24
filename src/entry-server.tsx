70137b3dc3e070937ff8e4c10b1c3380021b8119
Andrey Vetrov <a.vetrov@gmail.com>
2025-06-24 14:14:51 +0300

    Libs update

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom/server'
 import { App } from './App'
+import { addSlash } from './utils/url.ts'
 
 export function render (url: string) {
   const html = ReactDOMServer.renderToString(
-    <StaticRouter location={url}>
+    <StaticRouter location={addSlash(url)}>
       <App />
     </StaticRouter>
   )
