9690865c7f2447b49324cedc497dc021817e8c44
Andrey Vetrov <a.vetrov@gmail.com>
2025-06-24 14:20:54 +0300

    Libs update

---
+++ src/utils/url.ts
@@ -0,0 +1,1 @@
+export const addSlash = (url: string): string => url.at(0) === '/' ? url : `/${url}`
--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-/*  resolve: {
-    // MUI + SSR issue https://github.com/vikejs/vike/discussions/901#discussioncomment-6167919
-    alias: {
-      '@mui/material': '@mui/material/node',
-      '@mui/styled-engine': '@mui/styled-engine/node',
-      '@mui/system': '@mui/system/esm',
-      '@mui/base': '@mui/base/node',
-      '@mui/utils': '@mui/utils/node',
-      '@mui/lab': '@mui/lab/node',
-    }
-  },
-
- */
-  ssr: {noExternal: ["@mui/material", "@mui/utils", "@mui/base", "@mui/icons-material"]},
   plugins: [react()]
 })
