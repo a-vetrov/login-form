abf9ab0ba94cd34ab635eec114055d18dd1021c0
Andrey Vetrov <a.vetrov@gmail.com>
2025-12-16 00:15:20 +0300

    Websocket client added

---
+++ src/utils/hooks/use-websockets.ts
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux'
+import { userInfoSelector } from '../../store/selectors/user-info.ts'
+import { useEffect } from 'react'
+
+export const useWebsockets = (): void => {
+  const userInfo = useSelector(userInfoSelector)
+
+  useEffect(() => {
+    if (!userInfo || !window) {
+      return
+    }
+
+    const url = `ws//${window.location.host}`
+
+    const socket = new WebSocket(url)
+
+    socket.onopen = function (event) {
+      console.log('Соединение WebSocket установлено!', event)
+    }
+
+    socket.onmessage = function (event) {
+      console.log('Получены данные:', event.data)
+    }
+
+    socket.onerror = function (error) {
+      console.error('Ошибка WebSocket:', error)
+    }
+
+    socket.onclose = function (event) {
+      console.log('Соединение WebSocket закрыто:', event.code, event.reason)
+    }
+  }, [userInfo])
+}
